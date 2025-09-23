72c5bec66d67d7bcf87c3fd0be77514ba0c51931
sumchive <haon0319@naver.com>
2025-09-23 13:43:19 +0900

    Initial Commit

---
+++ SettleSupport/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SettleSupport
+//
+//  Created by mandoo on 9/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SettleSupportTests/SettleSupportTests.swift
@@ -0,0 +1,17 @@
+//
+//  SettleSupportTests.swift
+//  SettleSupportTests
+//
+//  Created by mandoo on 9/23/25.
+//
+
+import Testing
+@testable import SettleSupport
+
+struct SettleSupportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
